e4730a49d440e6ded422ab0af95504c8a9728507
lauslim12 <nicholasdwiarto@yahoo.com>
2021-03-28 21:02:18 +0700

    feat: Initialize base page

---
+++ pages/_app.tsx
@@ -0,0 +1,23 @@
+import 'focus-visible/dist/focus-visible';
+
+import { AppProps } from 'next/app';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+const fallbackFonts =
+  '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <ChakraProvider
+    resetCSS
+    theme={extendTheme({
+      fonts: {
+        body: `Quicksand, ${fallbackFonts}`,
+        heading: `Quicksand, ${fallbackFonts}`,
+      },
+    })}
+  >
+    <Component {...pageProps} />
+  </ChakraProvider>
+);
+
+export default App;
---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import { memo } from 'react';
+
+const Home = () => (
+  <div>
+    <p>Hello World!</p>
+  </div>
+);
+
+export default memo(Home);
