9d18378482e4393ccc2f7def91783672532925b7
lauslim12 <nicholasdwiarto@yahoo.com>
2021-03-29 10:57:53 +0700

    fix: Fix 'Fumino' layout

--- components/Home/Fumino.tsx
+++ components/Home/Fumino.tsx
@@ -1,12 +1,16 @@
-import { Heading, Text, VStack } from '@chakra-ui/react';
+import { Flex, Heading, Text, VStack } from '@chakra-ui/layout';
 import { memo } from 'react';
 
 const Fumino = () => (
-  <VStack textAlign="center" minH="30vh">
-    <Heading as="h1">Fumi-no</Heading>
-    <Text as="h2">If written 史乃, it means 'Your History'.</Text>
-    <Text as="h2">So, the question is, what is the good thing that happened to you today?</Text>
-  </VStack>
+  <Flex textAlign="center" minH="100vh" align="center" justify="center">
+    <VStack spacing={3}>
+      <Heading as="h1" size="4xl">
+        Fumi-no
+      </Heading>
+      <Text as="h2">If written 史乃, it means 'Your History'.</Text>
+      <Text as="h2">So, the question is, what is the good thing that happened to you today?</Text>
+    </VStack>
+  </Flex>
 );
 
 export default memo(Fumino);
