049dbc3338fc62d763f55b58a34cdb010831817a
lauslim12 <nicholasdwiarto@yahoo.com>
2021-03-29 22:06:26 +0700

    feat: Implement modal to add blessing

---
+++ components/AdditionForm.tsx
@@ -0,0 +1,63 @@
+import {
+  Box,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  Radio,
+  RadioGroup,
+  VStack,
+  Wrap,
+  WrapItem,
+} from '@chakra-ui/react';
+import { Dispatch, memo, SetStateAction } from 'react';
+import { Colors } from '../types/Enums';
+import { radioValues } from '../utils/constants';
+
+type Props = {
+  blessing: string;
+  color: Colors;
+  setBlessing: Dispatch<SetStateAction<string>>;
+  setColor: Dispatch<SetStateAction<Colors | string>>;
+};
+
+const AdditionForm = ({ blessing, color, setBlessing, setColor }: Props) => (
+  <VStack spacing={4}>
+    <FormControl id="blessing" isRequired>
+      <FormLabel>Good thing that happened today.</FormLabel>
+      <Input
+        type="text"
+        name="blessing"
+        placeholder="I helped my co-worker with computer problems!"
+        focusBorderColor="green.300"
+        errorBorderColor="red.300"
+        autoComplete="off"
+        value={blessing}
+        onChange={({ currentTarget: { value } }) => setBlessing(value)}
+      />
+      <FormHelperText>Write one good thing that happened to you today!</FormHelperText>
+    </FormControl>
+
+    <FormControl id="color" isRequired>
+      <FormLabel>Your color for your good thing.</FormLabel>
+
+      <RadioGroup value={color} onChange={setColor} name="color">
+        <Wrap>
+          {radioValues.map((value) => (
+            <WrapItem key={value}>
+              <Radio value={value} colorScheme="red">
+                <Box bg={`${value}.300`} p={1} borderRadius="md">
+                  {value}
+                </Box>
+              </Radio>
+            </WrapItem>
+          ))}
+        </Wrap>
+      </RadioGroup>
+
+      <FormHelperText>Choose one of the colors that represents this blessing!</FormHelperText>
+    </FormControl>
+  </VStack>
+);
+
+export default memo(AdditionForm);
---
+++ components/AdditionModal.tsx
@@ -0,0 +1,102 @@
+import {
+  Alert,
+  AlertIcon,
+  Button,
+  ButtonGroup,
+  chakra,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useToast,
+  VStack,
+} from '@chakra-ui/react';
+import { Dispatch, FormEvent, memo, SetStateAction, useState } from 'react';
+import { Data } from '../types/Data';
+import { Colors, Days, Months } from '../types/Enums';
+import AdditionForm from './AdditionForm';
+
+type Props = {
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
+  data: Data[];
+  setData: Dispatch<SetStateAction<Data[]>>;
+};
+
+const AdditionModal = ({ open, setOpen, data, setData }: Props) => {
+  const [blessing, setBlessing] = useState('');
+  const [color, setColor] = useState('red' as Colors);
+  const toast = useToast();
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    const currentDate = new Date();
+    const newData: Data = {
+      id: `${currentDate.getMonth()}-${currentDate.getFullYear()}-${color}-${currentDate.getTime()}`,
+      blessing,
+      color,
+      day: currentDate.getDate() as Days,
+      month: currentDate.getMonth() as Months,
+      year: currentDate.getFullYear(),
+    };
+
+    setData((prevData) => [...prevData, newData]);
+    setOpen(false);
+
+    toast({
+      title: 'Data added!',
+      description: 'Your data has been added!',
+      status: 'success',
+      isClosable: true,
+      duration: 5000,
+    });
+  };
+
+  return (
+    <Modal isOpen={open} onClose={() => setOpen(false)} size="lg">
+      <ModalOverlay />
+      <ModalContent as="form" onSubmit={handleSubmit}>
+        <ModalHeader>Add Data</ModalHeader>
+        <ModalCloseButton />
+
+        <ModalBody>
+          <VStack align="stretch" spacing={4}>
+            <Alert status="warning">
+              <AlertIcon />
+              Input is not sanitized. Corrupted input could make the application crash! Delete the
+              local storage to fix the problem!
+            </Alert>
+
+            <chakra.div>
+              <chakra.p>Input your data here!</chakra.p>
+            </chakra.div>
+
+            <AdditionForm
+              blessing={blessing}
+              color={color}
+              setBlessing={setBlessing}
+              setColor={setColor as Dispatch<SetStateAction<Colors | string>>}
+            />
+          </VStack>
+        </ModalBody>
+
+        <ModalFooter>
+          <ButtonGroup>
+            <Button colorScheme="green" type="submit">
+              Add Data
+            </Button>
+            <Button colorScheme="blue" onClick={() => setOpen(false)}>
+              Close
+            </Button>
+          </ButtonGroup>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default memo(AdditionModal);
