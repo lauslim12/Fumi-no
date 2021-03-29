68321b269fd3f7c47b3bf6c1767cbb2a7f7171e2
lauslim12 <nicholasdwiarto@yahoo.com>
2021-03-29 10:58:10 +0700

    fix: Fix text alignment

--- components/Home/Greetings.tsx
+++ components/Home/Greetings.tsx
@@ -1,9 +1,11 @@
-import { Heading, VStack } from '@chakra-ui/react';
+import { Heading, VStack } from '@chakra-ui/layout';
 import { memo } from 'react';
 
 const Greetings = () => (
   <VStack textAlign="center">
-    <Heading as="h1">Welcome back, Nicholas!</Heading>
+    <Heading as="h1" size="2xl">
+      Welcome back, Nicholas!
+    </Heading>
   </VStack>
 );
 
