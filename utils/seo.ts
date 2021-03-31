24e86b5a7a69ab824a4e14be78799152ac6bf928
lauslim12 <nicholasdwiarto@yahoo.com>
2021-03-31 11:15:42 +0700

    feat: Add information for SEO

---
+++ utils/seo.ts
@@ -0,0 +1,46 @@
+const pageTitles = {
+  main: 'Fumino',
+  front: 'Fumino - Front Page',
+  motivation: 'Fumino - Motivation',
+};
+
+export const SEO = {
+  title: pageTitles.main,
+  description: 'Fumino is an open-source, web version of Happiness Jar! No registration needed!',
+  openGraph: {
+    title: pageTitles.main,
+    description: 'Fumino is an open-source, web version of Happiness Jar! No registration needed!',
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://fumi-no.com/home',
+    site_name: 'Fumino',
+    images: [
+      {
+        url: 'https://fumi-no.com/fumino-seo.png',
+        width: 512,
+        height: 512,
+        alt: 'Icon for Fumino',
+      },
+    ],
+  },
+};
+
+export const frontSEO = {
+  ...SEO,
+  title: pageTitles.front,
+  openGraph: {
+    ...SEO.openGraph,
+    title: pageTitles.front,
+    url: 'https://fumi-no.com/',
+  },
+};
+
+export const motivationSEO = {
+  ...SEO,
+  title: pageTitles.motivation,
+  openGraph: {
+    ...SEO.openGraph,
+    title: pageTitles.motivation,
+    url: 'https://fumi-no.com/motivation',
+  },
+};
