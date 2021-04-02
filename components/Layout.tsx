6cecca7fab840c897aa9610eac54d416b3730e9e
lauslim12 <nicholasdwiarto@yahoo.com>
2021-04-02 22:03:38 +0700

    fix: Only import essential fonts for faster loading speed

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,8 +14,8 @@ type Props = {
 
 const Layout = ({ children, title, isFullPage = false }: Props) => (
   <>
-    <GoogleFonts href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" />
-    <GoogleFonts href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" />
+    <GoogleFonts href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;500;600&display=swap" />
+    <GoogleFonts href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;700&display=swap" />
 
     <Head>
       <meta charSet="UTF-8" />
