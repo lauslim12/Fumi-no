7c9f0c05b2784f3b29d3271c8d9baf8448a53524
lauslim12 <nicholasdwiarto@yahoo.com>
2021-04-04 22:30:15 +0700

    feat: Implement clock

--- components/Main/Greetings.tsx
+++ components/Main/Greetings.tsx
@@ -1,16 +1,31 @@
 import { Heading, VStack } from '@chakra-ui/layout';
-import { memo, useContext } from 'react';
+import { memo, useContext, useEffect, useState } from 'react';
 import UserContext from '../../utils/config';
 
 const Greetings = () => {
   const {
     state: { name },
   } = useContext(UserContext);
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => setCurrentTime(new Date()), 1000);
+
+    return () => clearInterval(timer);
+  });
 
   return (
-    <VStack textAlign="center">
+    <VStack textAlign="center" spacing={3}>
       <Heading as="h1" size="2xl">
         {`Welcome back, ${name}!`}
+      </Heading>
+
+      <Heading as="h2" size="md">
+        Hello! Tell me about your day! Write your stories here!
+      </Heading>
+
+      <Heading as="h5" size="md" mb={5}>
+        {`Time: ${currentTime.toLocaleDateString()} — ${currentTime.toLocaleTimeString('en-GB')}`}
       </Heading>
     </VStack>
   );
