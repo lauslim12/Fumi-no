b98e2f2ef5cd8fe242959db9f55bf3ae32c144d4
lauslim12 <nicholasdwiarto@yahoo.com>
2021-04-05 11:16:58 +0700

    feat: Add function to serialize strings to booleans

--- utils/json.ts
+++ utils/json.ts
@@ -1,14 +1,31 @@
+/**
+ * This function is used to validate a string, whether it is a valid JSON or not.
+ * Neither JSON.parse(false) or JSON.parse(1234) throw errors, hence the type-checking,
+ * JSON.parse(null) returns null, and typeof null === "object".
+ * We must check for that, too. Thankfully, null is falsey, so this function alone suffices.
+ *
+ * @param str - JSON string.
+ * @returns Boolean value whether the JSON is valid or not.
+ */
 export const jsonValidate = (str: string): boolean => {
   try {
     const parsedJSON = JSON.parse(str);
 
-    // Neither JSON.parse(false) or JSON.parse(1234) throw errors, hence the type-checking,
-    // JSON.parse(null) returns null, and typeof null === "object",
-    // so we must check for that, too. Thankfully, null is falsey, so this suffices.
     if (parsedJSON && typeof parsedJSON === 'object') return true;
   } catch (err) {
     // Ignored.
   }
 
   return false;
+};
+
+/**
+ * This function is used to transform 'true' (string) to 'true' (boolean) and in reverse.
+ * This is done as local storage can only store serializable values (usually strings).
+ *
+ * @param str - Either 'true' or 'false'
+ * @returns A boolean, symbolizing the boolean value of the 'true' or 'false'.
+ */
+export const isStringTrue = (str: 'true' | 'false'): boolean => {
+  return JSON.parse(str);
 };
