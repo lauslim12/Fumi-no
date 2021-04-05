f4e06141bc488fd73bcb9351e71c78cb0765201f
lauslim12 <nicholasdwiarto@yahoo.com>
2021-04-05 12:00:16 +0700

    feat: Implement custom header

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/:path*{/}?',
+        headers: [
+          {
+            key: 'X-Fumino-Application',
+            value: 'Alicia',
+          },
+        ],
+      },
+    ];
+  },
   reactStrictMode: true,
 };
